8ada91c556077cda9225885f08a71146a8fbbc7d
akchua <adrianjasperchua@gmail.com>
2016-09-11 11:05:13 +0800

    added account list

--- thesis/src/main/resources/public/app/viewmodels/admin/account.js
+++ thesis/src/main/resources/public/app/viewmodels/admin/account.js
@@ -1,12 +1,40 @@
-define(['knockout'], function (ko) {
+define(['knockout', 'modules/accountservice'], function (ko, accountService) {
     var Account = function() {
-    	this.test = ko.observable(4);
+    	this.accountList = ko.observable();
+    	
+    	this.searchKey = ko.observable();
+    	
+    	this.itemsPerPage = ko.observable(10);
+    	this.totalItems = ko.observable();
+    	this.currentPage = ko.observable(1);
+    	this.currentPageSubscription = null;
     };
     
-    Account.prototype.change = function() {
+    Account.prototype.activate = function() {
     	var self = this;
     	
-    	self.test(self.test() + 1);
+    	self.currentPage(1);
+    	self.currentPageSubscription = self.currentPage.subscribe(function() {
+			self.refreshAccountList();
+		});
+		
+		self.refreshAccountList();
+    };
+    
+    Account.prototype.refreshAccountList = function() {
+    	var self = this;
+    	
+    	accountService.getAccountList(self.currentPage(), self.searchKey()).done(function(data) {
+    		self.accountList(data.list);
+    		self.totalItems(data.total);
+    	});
+    };
+    
+    Account.prototype.search = function() {
+    	var self = this;
+    	
+    	self.currentPage(1);
+    	self.refreshAccountList();
     };
     
     return Account;
